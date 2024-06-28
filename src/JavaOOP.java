ec855dd3c26dd6f62536cb22ac2c441f2d737e06
Klolarion <85093525+klolarion@users.noreply.github.com>
2024-06-28 12:11:10 +0900

    제너릭, 캐스팅, 상속 등

--- src/JavaOOP.java
+++ src/JavaOOP.java
@@ -1,38 +1,76 @@
 public class JavaOOP {
     public static void main(String[] args) {
-        Person person1 = new Person();
-        Person person2 = new Person("lee", 30);
-        person1.setAge(25);
-        person1.setName("kim");
+//        Person person1 = new Person();
+//        Person person2 = new Person("lee", 30);
+//        person1.setAge(25);
+//        person1.setName("kim");
+//
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//
+//        System.out.println(Person.from);
+//
+//        Cat cat1 = new Cat();
+//        Dog dog1 = new Dog();
+//
+//        cat1.sound();
+//        dog1.sound();
+//
+//
+//        Fish fish1 = new Fish();
+//        System.out.println(fish1.place);
+//        fish1.species();
+//
+//        Shark shark1 = new Shark();
+//        System.out.println(shark1.place);
+//        shark1.species();
+//        shark1.name();
+//
+//        Shark shark2 = new Shark("아기상어");
+//
+//        System.out.println(shark2.name);
+//
+//        SUV suv1 = new SUV();
+//        suv1.wheel();
+//        suv1.sound();
 
-        System.out.println(person1.getName());
-        System.out.println(person2.getName());
 
-        System.out.println(Person.from);
+//        Animal2 a2 = new Cow();
+//        Animal2 a3 = new Horse();
+//        Cow cow = new Cow();
+//
+//        a2.eat();
+//        a2.walk();
+//
+//        System.out.println("-----------");
+//
+//        a3.eat();
+//        a3.walk();
+//
+//        System.out.println("-----------");
+//
+//
+//        //부모인스턴스일때 부모필드출력
+//        System.out.println(a2.place);
+//        //자식인스턴스일때 자식필드출력
+//        System.out.println(cow.place);
 
-        Cat cat1 = new Cat();
-        Dog dog1 = new Dog();
-
-        cat1.sound();
-        dog1.sound();
+        Box<String> box1 = new Box<>();
+        box1.setItem("String");
+        Box<Integer> box2 = new Box<>();
 
 
-        Fish fish1 = new Fish();
-        System.out.println(fish1.place);
-        fish1.species();
-
-        Shark shark1 = new Shark();
-        System.out.println(shark1.place);
-        shark1.species();
-        shark1.name();
-
-        Shark shark2 = new Shark("아기상어");
-
-        System.out.println(shark2.name);
-
-        SUV suv1 = new SUV();
-        suv1.wheel();
-        suv1.sound();
+        Animal2 animal1 = new Animal2();
+        Cow cow1 = new Cow();
+        System.out.println(cow1.place);
+        cow1.eat("건초");
+        //부모타입으로 업캐스팅. 문제없음.
+        Animal2 animal2 = cow1;
+        System.out.println(animal2.place);
+        animal2.eat();
+        //자식타입으로 다운캐스팅. 문제발생.
+        Cow cow2 = (Cow) animal1;
+        System.out.println(cow2.place);
     }
 }
 
@@ -141,5 +179,60 @@ class SUV extends Car{
     @Override
     void sound() {
         System.out.println("달달달달");
+    }
+}
+
+
+class Animal2{
+    String place = "공원";
+    void walk() {
+        System.out.println("걸어");
+        System.out.println("소입니까? " + (this instanceof Cow));
+        System.out.println("말입니까? " + (this instanceof Horse));
+
+        if(this instanceof Cow){
+            System.out.println("소몰이");
+        }else if(this instanceof Horse){
+            System.out.println("다그닥");
+        }
+    }
+
+    void eat(){
+        System.out.println("냠냠");
+    }
+}
+class Cow extends Animal2{
+    String place = "축사";
+    void eat(String food){
+        System.out.println(food + "를 냠냠");
+    }
+
+}
+class Horse extends Animal2{
+    @Override
+    void eat() {
+        System.out.println("풀머겅");
+    }
+
+}
+
+
+class Box<T>{
+    private T item;
+
+    void setItem(T item){
+        this.item = item;
+    }
+
+    T getItem(){
+        return item;
+    }
+}
+//어떤 타입의 배열이 오더라도 출력할 수 있음
+class Utils {
+    public static <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.println(element);
+        }
     }
 }
